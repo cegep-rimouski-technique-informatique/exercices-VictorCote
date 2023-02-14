998a0aaa81e8bb2da2835f89d818ec7c42fe9a23
VictorCote <victor.cote3991@gmail.com>
2023-02-14 12:57:43 -0800

    exercice#12--javascript-dom

---
+++ 12-javascript-DOM/scripts/script.js
@@ -0,0 +1,32 @@
+// 1. Dessinez le modèle par arborescence du DOM (Document Object Model) de index.html
+
+// Éxecuter le Javascript après la création du DOM
+window.onload = () => {
+  // 2. Remplacez le texte du titre par "DOMaine des Arbres"
+  document.getElementById("titre").textContent = "DOmaine des Arbres";
+
+  // 3. Changez l'ordre de présentation des images avec flex
+  const elem = document.getElementsByClassName("container")[0];
+  elem.setAttribute("style", "display: flex;flex-direction: row;");
+
+  // 4. Ajoutez le point "Terre" à la liste d'éléments
+  let li = document.createElement("li");
+  li.appendChild(document.createTextNode("Terre"));
+  document.getElementsByTagName("ul")[0].appendChild(li);
+
+  // 5. Ajoutez l'attribut href manquant à la balise lien de l'image d'arbre.
+  // avec la valeur  href="https://www.gardeningknowhow.com/wp-content/uploads/2017/07/hardwood-tree.jpg"
+  // Avec le sélecteur suivant, utilisez un "accès par relation" pour faire votre modification.
+  const imageSansLien = document.querySelectorAll(
+    'img[src="https://www.gardeningknowhow.com/wp-content/uploads/2017/07/hardwood-tree.jpg"]'
+  )[0];
+  imageSansLien.setAttribute(
+    "href",
+    "https://www.gardeningknowhow.com/wp-content/uploads/2017/07/hardwood-tree.jpg"
+  );
+
+  // 6. Modifiez la police du texte pour toutes les éléments de la liste
+  document
+    .getElementsByTagName("ul")[0]
+    .setAttribute("style", "font-family: 'Courier New', Courier, monospace;");
+};
